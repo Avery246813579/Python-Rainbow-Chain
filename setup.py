0ad2ca3424e0d2570fce5a39020920a0a8f95294
Avery Durrant <avery246813579@gmail.com>
2017-10-20 12:09:47 -0700

    Last attempt

--- setup.py
+++ setup.py
@@ -1,17 +1,16 @@
 from distutils.core import setup
-from setuptools import find_packages
 
 setup(
     name='RainbowChain',
-    packages=find_packages('lib'),  # this must be the same as the name above
-    package_dir={'': 'lib'},
+    packages='RainbowChain',  # this must be the same as the name above
     license="MIT License",
-    version='0.3',
+    version='0.4',
     include_package_data=True,
     description='Implementing Middle Out (Indexing) to the Markov Chain',
     author='Avery Durrant',
     author_email='avery@frostbyte.co',
     url='https://github.com/Avery246813579/Python-Rainbow-Chain',
+    download_url='https://github.com/Avery246813579/Python-Rainbow-Chain/0.1',
     keywords=['Markov Chain', 'Middle Out', 'Markov Model'],
     classifiers=[],
 )
