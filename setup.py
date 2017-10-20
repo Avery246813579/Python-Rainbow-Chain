b8dae0592783f675274884dd3ac081aec9446db5
Avery Durrant <avery246813579@gmail.com>
2017-10-20 11:23:33 -0700

    We changed the name

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='rainbow_chain',
     packages=['Rainbow_Chain'],  # this must be the same as the name above
-    version='0.2',
+    version='0.1',
     description='Implementing Middle Out (Indexing) to the Markov Chain',
     author='Avery Durrant',
     author_email='avery@frostbyte.co',
