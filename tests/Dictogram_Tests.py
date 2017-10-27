9da1ae5072f71439d04c052ddfb061f7b3fddd23
Avery Durrant <avery246813579@gmail.com>
2017-10-27 15:36:10 -0700

    We made backwards forwards run at once

--- tests/Dictogram_Tests.py
+++ tests/Dictogram_Tests.py
@@ -1,7 +1,8 @@
 from RainbowChain import Dictogram
-words = 'One fish two fish red fish blue fish.'.split()
 
-d = Dictogram(words, 2)
+words = 'One fish two fish red fish blue fish. One dog two dog red dog blue dog'.split()
+
+d = Dictogram(words, 3)
 
 # file_reader = FileParser('static/test_data.txt')
 
@@ -21,18 +22,71 @@ d = Dictogram(words, 2)
 
 
 print(d.backwards)
-# {('fish', 'blue'): Histogram[{'fish': 1}],
+print(d.sentence_ends)
+# {('dog', 'blue'): Histogram[{'dog': 1}],
+# ('blue', 'dog'): Histogram[{'red': 1}],
+# ('dog', 'red'): Histogram[{'dog': 1}],
+# ('red', 'dog'): Histogram[{'two': 1}],
+# ('dog', 'two'): Histogram[{'dog': 1}],
+# ('two', 'dog'): Histogram[{'One': 1}],
+# ('dog', 'One'): Histogram[{'[SPLIT]': 1}],
+# ('fish', 'blue'): Histogram[{'fish': 1}],
 # ('blue', 'fish'): Histogram[{'red': 1}],
 # ('fish', 'red'): Histogram[{'fish': 1}],
 # ('red', 'fish'): Histogram[{'two': 1}],
 # ('fish', 'two'): Histogram[{'fish': 1}],
 # ('two', 'fish'): Histogram[{'One': 1}]}
 
+# [('dog', 'blue'), ('fish', 'blue')]
 
 
-# {('two', 'fish'): [Histogram: {'fish': 1}],
+# {('two', 'fish'): [Histogram: {'One': 1}],
+# ('fish', 'fish'): [Histogram: {'fish': 1}],
 # ('fish', 'red'): [Histogram: {'two': 1}],
-#  ('red', 'fish'): [Histogram: {'fish': 1}],
-# ('fish', 'blue'): [Histogram: {'red': 1, 'fish': 1}]}
+# ('red', 'fish'): [Histogram: {'fish': 1}],
+# ('fish', 'blue'): [Histogram: {'red': 1}],
+# ('blue', 'fish'): [Histogram: {'fish': 1}],
+# ('dog', 'One'): [Histogram: {'[SPLIT]': 1}],
+# ('dog', 'two'): [Histogram: {'One': 1}],
+#  ('two', 'dog'): [Histogram: {'dog': 1}],
+# ('dog', 'red'): [Histogram: {'two': 1}],
+# ('red', 'dog'): [Histogram: {'dog': 1}],
+# 2('dog', 'blue'): [Histogram: {'red': 1}], ('blue', 'dog'): [Histogram: {'dog': 1}], ('fish', 'One'): [Histogram: {'[SPLIT]': 1}]}
+# [('fish', 'One'), ('blue', 'dog')]
 
 
+
+# {('dog',): Histogram[{'blue': 1, 'red': 1, 'two': 1, 'One': 1}],
+# ('blue',): Histogram[{'dog': 1, 'fish': 1}],
+# ('red',): Histogram[{'dog': 1, 'fish': 1}],
+# ('two',): Histogram[{'dog': 1, 'fish': 1}],
+# ('One',): Histogram[{'[SPLIT]': 1}],
+# ('fish',): Histogram[{'blue': 1, 'red': 1, 'two': 1, 'One': 1}]}
+# [('dog',), ('fish',)]
+
+
+# {('fish',): [Histogram: {'One': 1, 'two': 1, 'red': 1, 'blue': 1}],
+# {('two', 'fish'): [Histogram: {'One': 1}], ('fish', 'fish'): [Histogram: {'fish': 1}], ('fish', 'red'): [Histogram: {'two': 1}], ('red', 'fish'): [Histogram: {'fish': 1}], ('fish', 'blue'): [Histogram: {'red': 1}], ('blue', 'fish'): [Histogram: {'fish': 1}], ('dog', 'One'): [Histogram: {'[SPLIT]': 1}], ('dog', 'two'): [Histogram: {'One': 1}], ('two', 'dog'): [Histogram: {'dog': 1}], ('dog', 'red'): [Histogram: {'two': 1}], ('red', 'dog'): [Histogram: {'dog': 1}], ('dog', 'blue'): [Histogram: {'red': 1}], ('blue', 'dog'): [Histogram: {'dog': 1}], ('fish', 'One'): [Histogram: {'[SPLIT]': 1}]}
+# [('fish', 'One'), ('blue', 'dog')]
+
+
+
+
+
+
+
+
+
+# {('dog', 'blue', 'dog'): Histogram[{'red': 1}],
+# ('blue', 'dog', 'red'): Histogram[{'dog': 1}],
+# ('dog', 'red', 'dog'): Histogram[{'two': 1}],
+# ('red', 'dog', 'two'): Histogram[{'dog': 1}],
+# ('dog', 'two', 'dog'): Histogram[{'One': 1}],
+# ('two', 'dog', 'One'): Histogram[{'[SPLIT]': 1}],
+# ('fish', 'blue', 'fish'): Histogram[{'red': 1}],
+# ('blue', 'fish', 'red'): Histogram[{'fish': 1}],
+# ('fish', 'red', 'fish'): Histogram[{'two': 1}],
+# ('red', 'fish', 'two'): Histogram[{'fish': 1}],
+# ('fish', 'two', 'fish'): Histogram[{'One': 1}]}
+
+# [('dog', 'blue', 'dog'), ('fish', 'blue', 'fish')]
